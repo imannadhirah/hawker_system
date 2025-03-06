25b41fd8350eacec2fea4a3aee7a3a48064757f5
iman <nadhirah.iman2004@gmail.com>
2025-03-06 14:28:51 +0800

    Set mainscreen as homepage

--- manager/settings.py
+++ manager/settings.py
@@ -117,15 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'polls/static'),  # ✅ Tells Django where to find static files
-]
+# This is where collected static files will be stored
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+# Optional: Add this so Django looks for static files in the 'static' folder in each app
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'polls', 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
