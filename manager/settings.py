66f6cdb03c5d82dfc78dfb8308bb29e233d019f5
iman <nadhirah.iman2004@gmail.com>
2025-03-06 14:20:36 +0800

    Set mainscreen as homepage

--- manager/settings.py
+++ manager/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'polls/static'),  # ✅ Tells Django where to find static files
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
